f76867606ec04602e31363fda83cc0b039db5227
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-06 21:01:00 -0500

    got my urls for the main app down

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('artists', views.ArtistView)
+router.register('songs', views.SongView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 
 ]
