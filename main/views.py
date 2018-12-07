db6a3afa472a22d313b4a69fb6bd6f2ad985387e
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-06 20:56:53 -0500

    got views for my app down and ready to go

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import ArtistSerializer, SongSerializer
+from .models import Artist, Song
+
+
+class ArtistView(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class SongView(viewsets.ModelViewSet):
+    queryset =  Song.objects.all()
+    serializer_class = SongSerializer
