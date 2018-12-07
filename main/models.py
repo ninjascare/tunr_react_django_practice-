f9e4a0fe90e74225b4b3108389a18d0f07d24ef6
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-12-06 20:40:58 -0500

    created a dunder method to be able to display the fields of my model into actual string

--- main/models.py
+++ main/models.py
@@ -8,8 +8,14 @@ class Artist(models.Model):
     photo_url = models.CharField(max_length=400)
     nationality = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Song(models.Model):
     title = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
     preview_url = models.CharField(max_length=400)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+
+    def __str__(self):
+        return self.title
